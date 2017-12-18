69c386fe9096f3abbb42c2737ddb08570b0b9623
Huwl <hwl2016@sina.com>
2017-12-18 23:07:05 +0800

    20171218 home submit

--- blog/admin.py
+++ blog/admin.py
@@ -4,4 +4,10 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from models import Article
 
-admin.site.register(Article)
+# admin.site.register(Article)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'pub_time')
+    list_filter = ('pub_time',)
+
+admin.site.register(Article, ArticleAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,42 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from . import models
 
 def index(request):
-    article = models.Article.objects.get(pk=1)
+    # article = models.Article.objects.get(pk=1)
+    articles = models.Article.objects.all()
     return render(request, 'index.html', {
-        'article': article
+        'articles': articles
     })
+
+def article_page(request, article_id):
+    article = models.Article.objects.get(pk=article_id)
+    return render(request, 'article_page.html', {'article': article})
+
+def edit_page(request, article_id):
+    if str(article_id) == '0':  # 新建文章
+        return render(request, 'edit_page.html')
+
+    # 修改文章
+    article = models.Article.objects.get(pk=article_id)
+    return render(request, 'edit_page.html', {'article': article})
+
+
+def edit_action(request):
+    title = request.POST.get('title', 'Title')  # Title是给一个默认值
+    content = request.POST.get('content', 'Content')  # Content是给一个默认值
+    article_id = request.POST.get('article_id', '0')  # Content是给一个默认值
+
+    if article_id == '0':   # 新建文章
+        models.Article.objects.create(title=title, content=content)
+        return HttpResponseRedirect('/blog/index/')
+
+    # 修改文章
+    article = models.Article.objects.get(pk=article_id)
+    article.title = title
+    article.content = content
+    article.save()
+    return HttpResponseRedirect('/blog/index/')
+
