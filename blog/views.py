f71bf5b04f981bebbb42cc197c2c5159be4e53fb
hwl2016 <hwl2016@sina.com>
2017-12-18 19:50:57 +0800

    20171218 submit

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,10 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 
 def index(request):
+    article = models.Article.objects.get(pk=1)
     return render(request, 'index.html', {
-        'hello': 'Hello, Blog!!!'
+        'article': article
     })
